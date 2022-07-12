810f5612534b3beaaae4e4aba605269836230f5a
Aderemi Adesoji <104841287+Rhemmymatician@users.noreply.github.com>
2022-07-12 21:56:25 +0000

    Attempt assignment

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let total = 0;
+  //for (let i= 0; i<arrayOfNumbers.length; i++){
+  // total+=arrayOfNumbers[i];
+  arrayOfNumbers.forEach((value) => {
+    total+= value;
+  });
+    return total;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let count = 0;
+  arrayOfNumbers.forEach((value) => {
+    if (value % 2 == 0) {
+      count++;
+    }
+  });
+  return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
